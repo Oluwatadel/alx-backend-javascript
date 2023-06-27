7cccb9ff0b1ad2c5b0006bedd79208388e1f3940
Gabriel Oluwatobi Adelesi <111093334+Oluwatadel@users.noreply.github.com>
2023-06-27 11:45:05 -0700

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,31 +1,23 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const user = {
-    status: 'pending',
-    value: '',
-  };
-  const photo = {
-    status: 'pending',
-    value: '',
-  };
-  
-  try {
-    const signup = await signUpUser(firstName, lastName);
-    user.status = 'fulfilled';
-    user.value = signup;
-  } catch (error) {
-    user.status = 'rejected';
-    user.value = error.toString();
-  }
-  try {
-    const upload = await uploadPhoto(fileName);
-    photo.status = 'fulfilled';
-    photo.value = upload;
-  } catch (error) {
-    photo.status = 'rejected';
-    photo.value = error.toString();
-  }
-  return [user, photo];
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName),
+    uploadPhoto(fileName)];
+
+  return Promise.allSettled(promises)
+    .then((values) => {
+      const res = [];
+      values.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          res.push(value);
+        } else {
+          res.push({
+            status: value.status,
+            value: `Error: ${value.reason.message}`,
+          });
+        }
+      });
+      return res;
+    });
 }
